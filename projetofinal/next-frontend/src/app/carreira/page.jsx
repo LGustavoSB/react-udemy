27e951e8055ef79d7029af49912061f688aa2121
LGustavoSB <luisgustavosbatista@gmail.com>
2024-01-15 00:33:11 -0300

    modal responsividade

--- projetofinal/next-frontend/src/app/carreira/page.jsx
+++ projetofinal/next-frontend/src/app/carreira/page.jsx
@@ -18,19 +18,19 @@ export default function Carreira(){
 
     return(
         <div className={style.container}>
+            {showModal && <Modal isVisible={showModal} onClose={() => setShowModal(false)}/>}
             <FormProvider {...methods}>
                 <div  id="form-vaga">  
                     <h1 className={style.title} >Formulário de Vaga</h1>
                     <form className={style.containerForm} noValidate onSubmit={e => e.preventDefault()}>
-                        <Input type={'text'} name={'inputName'} id={'name'} label={'Nome:'} validation={{required: {value: true,message: 'required'}}} />
-                        <Input type={'email'} name={'inputEmail'} id={'email'} label={'Email:'} validation={{required: {value: true,message: 'required'},pattern: {value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, message: 'Invalid email address'}}}/>
-                        <Input type={'url'} name={'inputPortfolio'} id={'portfolio'} label={'Link para portfólio:'} validation={{required: {value: true,message: 'required'}}}/>
-                        <Input type={'text'} name={'inputAbout'} id={'about'} label={'Por que você quer fazer parte da Jojos?'} validation={{required: {value: true,message: 'required'}}} />
+                        <Input type={'text'} name={'inputName'} id={'name'} label={'Nome:'} validation={{required: {value: true,message: 'Obrigatório'}}} />
+                        <Input type={'email'} name={'inputEmail'} id={'email'} label={'Email:'} validation={{required: {value: true,message: 'Obrigatório'},pattern: {value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, message: 'Email Inválido'}}}/>
+                        <Input type={'url'} name={'inputPortfolio'} id={'portfolio'} label={'Link para portfólio:'} validation={{required: {value: true,message: 'Obrigatório'}}}/>
+                        <Input type={'text'} name={'inputAbout'} id={'about'} label={'Por que você quer fazer parte da Jojos?'} validation={{required: {value: true,message: 'Obrigatório'}}} />
                         <button className={style.button} type="submit" onClick={onSubmit}>Enviar formulario</button>
                     </form>
                 </div>
             </FormProvider>
-            {showModal && <Modal isVisible={showModal} onClose={() => setShowModal(false)}/>}
         </div>
     )
 }   
--- projetofinal/next-frontend/src/components/ui/modal/page.jsx
+++ projetofinal/next-frontend/src/components/ui/modal/page.jsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import style from './modal.module.css'
+import Button from "../button/page";
 
 export default function Modal({isVisible, onClose}){
     if (!isVisible){return null}
 
     return(
         <div className={style.container}>
-            <button onClick={() => onClose()}>X</button>
-            <h1>Formulário enviado com sucesso</h1>
+            <h1 className={style.title}>Formulário enviado com sucesso</h1>
             <Image src={'/vector.svg'} width={100} height={100} alt="Formulario enviado"/>
+            <button onClick={() => onClose()} className={style.closeButton}><Button text={'Fechar'}/></button>
         </div>
     )
 }
